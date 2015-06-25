91c6bb27a0f3b4bbcf72355e243b9ef80feee25c
Angel Fentanez <j.angel.fentanez@gmail.com>
2015-06-25 19:46:22 +0000

    [REF] openacademy: Added copy method into inherit

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -28,3 +28,15 @@ class Course(models.Model):
          'UNIQUE(name)',
          "The course title must be unique"),
     ]
+
+    @api.one # api.ode send default params: cr, uid, id context
+    def copy(self, default=None):
+        default['name'] = self.name + ' (copy)'
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}%".format(self.name)
+        else:
+            new_name = u"Copy of {}%".format(self.name, copied_count)
+        return super(Course, self).copy(default)
+        
