a5df15989aae77f06be218fbcfef90c1d3229106
Angel Fentanez <j.angel.fentanez@gmail.com>
2015-06-29 02:11:25 +0000

    [REF] openacademy: Adding menuitem to wizard and change to many2many field session wizar

--- openacademy/wizard/openacademy_wizard.py
+++ openacademy/wizard/openacademy_wizard.py
@@ -7,12 +7,13 @@ class Wizard(models.TransientModel):
     _name = 'openacademy.wizard'
 
     def _default_session(self):
-        return self.env['openacademy.session'].browse(self._context.get('active_id'))
-    session_wiz_id = fields.Many2one('openacademy.session',
-        string = "Session", required=True, default=_default_session)
+        return self.env['openacademy.session'].browse(self._context.get('active_ids'))
+    session_wiz_ids = fields.Many2many('openacademy.session',
+        string = "Sessions", required=True, default=_default_session)
     attendee_wiz_ids = fields.Many2many('res.partner', string="Attendees")
 
     @api.multi
     def subscribe(self):
-        self.session_wiz_id.attendee_ids |= self.attendee_wiz_ids
+        for session_wiz_id in self.session_wiz_ids:
+            session_wiz_id.attendee_ids |= self.attendee_wiz_ids
         return {}
