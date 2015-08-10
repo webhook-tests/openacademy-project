c1814f28cc288e914f234de0806e8d228b59feda
Angel Fentanez <j.angel.fentanez@gmail.com>
2015-08-10 00:44:39 +0000

    [REF] openacademy: fix flake8

--- openacademy/model/partner.py
+++ openacademy/model/partner.py
@@ -3,7 +3,7 @@ from openerp import fields, models
 
 
 class Partner(models.Model):
-    _inherit = 'res.partner' 
+    _inherit = 'res.partner'
     instructor = fields.Boolean("Instructor", default=False)
     session_ids = fields.Many2many('openacademy.session',
                                    string="Sesion como instructor",
